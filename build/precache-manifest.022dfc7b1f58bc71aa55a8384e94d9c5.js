ab602eb7cd9c60126be18b6cf1978e19e66aace9
RottK <r.kitchell815@gmail.com>
2021-03-11 15:34:47 -0600

    deploy in vercel

---
+++ build/precache-manifest.022dfc7b1f58bc71aa55a8384e94d9c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fd88f3e2e23a805ccedec39a1d3635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "058de64bd92ae17f4bc2",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058de64bd92ae17f4bc2",
+    "url": "/static/js/main.ebf6db71.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
